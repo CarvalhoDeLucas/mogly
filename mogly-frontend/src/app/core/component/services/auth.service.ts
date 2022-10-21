cbcf61c0c7c9c22fa36be642598b0a2cc4f2140a
lecarvalho01 <lucaseduardode.carvalho@philips.com>
2022-10-21 08:05:06 -0300

    Fazendo a autenticação

---
+++ mogly-frontend/src/app/core/component/services/auth.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { catchError, Observable, map, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private url: string = 'http://localhost:3000';
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+  ) { }
+
+  public sing(payload: { email: string, password: string }): Observable<any> {
+    return this.http.post<{ token: string }>(`${this.url}/sing`, payload).pipe(
+      map((res) => {
+        localStorage.removeItem('access_token');
+        localStorage.setItem('access_token', res.token);
+        return this.router.navigate(['home']);
+      }),
+      catchError((err) => {
+        if (err.error.message) {
+          return throwError(() => err.error.message);
+        }
+
+        return throwError(() => 'Inoperante, tente novamente mais tarde!');
+      }),
+    );
+  }
+
+  public logout() {
+    localStorage.removeItem('access_token');
+    return this.router.navigate(['login']);
+  }
+
+  public isAuthenticated(): boolean {
+    const token = localStorage.getItem('access_token');
+
+    if (!token) return false;
+
+    const jwtHelper = new JwtHelperService();
+    return !jwtHelper.isTokenExpired(token);
+  }
+}
--- mogly-frontend/src/app/pages/registration/registration.component.ts
+++ mogly-frontend/src/app/pages/registration/registration.component.ts
@@ -48,4 +48,8 @@ export class RegistrationComponent implements OnInit {
     }
   }
 
+  public clear() {
+    this.msgError = '';
+  }
+
 }
